fd433017516cb5f410b10b72c987cfabd5911199
David De Keersmaecker <David@Davids-MacBook-Pro.local>
2015-10-09 16:09:31 +0200

    fix bug in todoRepo.remove unit test

--- mvc-todo-refactor/js/todoRepo.spec.js
+++ mvc-todo-refactor/js/todoRepo.spec.js
@@ -1,24 +1,30 @@
 describe('to do service', function() {
-    beforeEach(function() {
-        todoRepo.init();
-    });
-
     describe('add an item', function() {
+        beforeEach(function() {
+            todoRepo.init();
+        });
         it("should return 1", function() {
             todoRepo.add('abc');
             var todos = todoRepo.getList();
             expect(todos.length).toBe(1);
 
             expect(todos[0].title).toBe('abc');
+
+            expect(todoRepo.getList().length).toBe(1);
         });
     });
 
     describe('added an item and removed it', function() {
+        beforeEach(function() {
+            todoRepo.init();
+        });
         it("should return 0", function() {
             todoRepo.add('abc');
+            todoRepo.remove(0);
             var todos = todoRepo.getList();
-            todoRepo.remove();
             expect(todos.length).toBe(0);
         });
     });
+
+    describe
 });
