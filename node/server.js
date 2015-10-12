7214c34919f47fcf9bf8079cb15aa777437f9209
David De Keersmaecker <David@Davids-MacBook-Pro.local>
2015-10-12 09:16:37 +0200

    add app.get response

--- node/server.js
+++ node/server.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var app = express();
 
+app.get('/', function(req, res, next) {
+    res.send('Hello, David.');
+});
+
 var server = app.listen(3000, function() {
     console.log('Express server listening on port 3000');
 });
