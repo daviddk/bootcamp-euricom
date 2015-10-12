4986b23b1685e83bcb63cd10a7dd1e9a417864f2
David De Keersmaecker <David@Davids-MacBook-Pro.local>
2015-10-12 12:26:35 +0200

    reset mvc node ex to working version

--- node/mvctodo-node/server.js
+++ node/mvctodo-node/server.js
@@ -1,56 +1,37 @@
 var express = require('express');
-var morgan = require('morgan');
-var path = require('path')
-var bodyParser = require('body-parser');
-var _ = require('underscore');
-var todos = require('./app/scripts/app.js');
+var path = require('path');
+var logger = require('morgan');
+var bodyParser = require("body-parser");
+var apiTest = require('./routes/test')
+var apiTodos = require('./routes/todos')
 
+// create app
 var app = express();
 
-console.log(todos.getList());
+// config
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
 
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
+// middleware
+app.use(logger('dev'));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(bodyParser.urlencoded(
+    { extended: true }
+));
 app.use(bodyParser.json());
-app.use(morgan('dev'));
 
-app.use(express.static(path.join(__dirname, 'app')));
+// routes
+app.use('/api', apiTest);
+app.use('/api', apiTodos);
 
-app.get('/api/todos', function(req, res, next) {
-    res.send(todos.getList());
+// sample view route
+app.get('/demo', function(req, res, next) {
+    res.render('index', { name: 'peter'});
 });
 
-app.get('/api/todos/:id', function(req, res, next) {
-    res.send(todos.get(req.params.id));
-});
-
-app.post('/api/todos', function(req, res, next) {
-    var todo = {
-        id: todos.uuid(),
-        title: req.body.title,
-        completed: req.body.completed
-    };
-    todos.add(todo);
-    res.send(todo);
-});
-
-/*app.put('/api/todos/:id', function(req, res, next) {
-    console.log(req.param.id);
-    console.log(req.body);
-    res.send({id: 12, name: 'david'});
-});*/
-
-app.delete('/api/todos/:id', function(req, res, next) {
-    console.log(req.params.id);
-    var todo = todos.get(req.params.id);
-    console.log(todo);
-    todos.remove(todo);
-    res.send(todo);
-});
-
-var port = process.env.PORT || 3000;
-
-var server = app.listen(port, function() {
-    console.log('Express server listening on port: ' + server.address().port);
+// listening
+var server = app.listen(app.get('port'), function() {
+  console.log('Express server listening on port: '
+                    + server.address().port);
 });
