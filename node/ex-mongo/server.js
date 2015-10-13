cbe37bf631d700047bc2ed2127e75a71efcdbdb3
David De Keersmaecker <David@Davids-MacBook-Pro.local>
2015-10-13 09:17:25 +0200

    init server.js

---
+++ node/ex-mongo/server.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var logger = require('morgan');
+
+//config
+var app = express();
+app.set('port', process.env.PORT || 3000);
+
+//middleware
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded(
+    { extended: true }
+));
+
+//routes
+var obj = [
+    {id: 1, name: 'david'},
+    {id: 2, name: 'joris'}
+]
+
+app.get('/', function(req, res, next) {
+    return res.status(200).send(obj);
+})
+
+//listen
+var server = app.listen(app.get('port'), function() {
+  console.log('Express server listening on port: '
+                    + server.address().port);
+});
