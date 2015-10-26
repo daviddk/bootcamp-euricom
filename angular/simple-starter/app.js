4cd3194e212cb723c634bbb82b5273805938a7df
David De Keersmaecker <david.dekeersmaecker@euri.com>
2015-10-26 09:37:19 +0100

    fix unit test with angular and angular-mocks

--- angular/simple-starter/app.js
+++ angular/simple-starter/app.js
@@ -1,6 +1,10 @@
 (function() {
 	'use strict';
 
-	angular.module('myApp', []);
-
+	angular.module('myApp', [])
+		.filter('upper', function() {
+			return function(input) {
+                return input.toUpperCase();
+            }
+		});
 })();
--- angular/simple-starter/my.spec.js
+++ angular/simple-starter/my.spec.js
@@ -1,7 +1,35 @@
 var expect = chai.expect;
 
 describe('module to test', function() {
-    it('should return true', function() {
-        expect(true).to.equal(true);
+    //lange versie, deze laadt de angular-mock module in
+    //beforeEach(function() {
+    //   angular.mock.module('app');
+    //});
+
+    //dit is hetzelfde als de code hierboven maar korter
+    beforeEach(module('myApp'));
+
+    beforeEach(inject(function(_upperFilter_){
+        upperFilter = _upperFilter_;
+    }));
+
+    it('should make input uppercase', function() {
+        expect(upperFilter('hello')).to.equal('HELLO');
     });
+
+    ////var $scope, $rootScope;
+    //beforeEach(inject(function(_$rootScope_) {
+    //    this.$rootScope = _$rootScope_;
+    //    this.$scope = this.$rootScope.$new();
+    //    this.$scope.name = 'test';
+    //    this.$rootScope.message = 'hello';
+    //}));
+    //
+    //it('should return true', function() {
+    //    expect(this.$scope.name).to.equal('test');
+    //});
+    //
+    //it('test', function() {
+    //    expect(this.$rootScope.message).to.equal('hello');
+    //})
 });
